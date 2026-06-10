c7f3798c81a79ef71207c59e57ef994e4910dc6e
Handpupa <johndoe@example.com>
2026-06-10 20:12:57 +0300

    abc

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -22,7 +22,7 @@ function UserList({users}){
   return(
     <ul>{users.map(user=>(
       <li>
-        key={user.id}{user.name}{user.age}
+        key={user.id},{user.name},{user.age}
       </li>
     ))}</ul>
   )
